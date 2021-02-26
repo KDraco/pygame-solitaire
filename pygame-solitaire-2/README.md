f46e11cef5203d5f2fbae9923c669ddaee12e067
KDraco <42868311+KDraco@users.noreply.github.com>
2021-02-26 12:43:36 -0500

    Add files via upload

---
+++ pygame-solitaire-2/README.md
@@ -0,0 +1,11 @@
+# Solitaire
+I made Solitaire in pygame
+
+# Requirements
+You need to have python 3. If you do not have pygame, installed it will be downloaded on first play.
+
+# To play
+Go to this folder in your terminal and run the game with the command
+```
+python3 solitaire.py
+```
---
+++ pygame-solitaire-2/src/constants.py
@@ -0,0 +1,25 @@
+#screen dimension
+WIDTH = 1200
+HEIGHT = 720
+#normal row cards
+BOTTOMROW_XY = (200, 180)
+#top row cards
+TOPROW_Y = 20
+#card dimension
+CARD_DIM = (98, 140)
+#spacing between cards
+CARD_SPACING = 20
+#different card suits
+CARD_SUITS = ["HEARTS", "DIAMONDS", "CLUBS", "SPADES"]
+#vertical offset of normal row holders
+CARD_HOLDER_VER_OFFSET = 30
+#small items on card size
+CARD_SMALL_ITEM_SIZE = (18, 21)
+
+MOUSEHOLDER_OFFSET = (0, CARD_HOLDER_VER_OFFSET)
+TOPLEFTHOLDER_OFFSET = (30, 0)
+TOPRIGHTHOLDER_OFFSET = (0, 0)
+BOTTOMROWHOLDER_OFFSET = (0, CARD_HOLDER_VER_OFFSET)
+
+#restart button
+RESTART_BUTTON_SIZE = (50, 50)
